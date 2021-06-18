070e5f0bb32af47bf8b852d6612bdfb20720330a
makafoy <142866042+makafoy@users.noreply.github.com>
2021-06-18 11:15:55 +0000

    little bug fix

--- PeacefulPie/NetManager.cs
+++ PeacefulPie/NetManager.cs
@@ -1,3 +1,29 @@
 using System;
 using System.IO;
-using
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using AustinHarris.JsonRpc;
+using UnityEngine;
+
+class NetworkEvent {
+	// contains request from client, space for reply from server,
+	// and a thread signaling thing so client can wait for reply
+	// we'll keep everything as json strings here, and do the conversion
+	// in the main thread
+	public string clientRequest;
+	public string? serverReply;
+	public AutoResetEvent serverReplied = new AutoResetEvent(false);
+	public NetworkEvent(string clientRequest) {
+		this.clientRequest = clientRequest;
+	}
+}
+
+
+
+public class NetManager : MonoBehaviour {
+	[Tooltip("Network port to listen on.")]
+	public int ListenPort = 9000;
+	[Tooltip("Network 
