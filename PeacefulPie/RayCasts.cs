4f9831271522b098f48788264e1b0a0124637d6f
makafoy <142866042+makafoy@users.noreply.github.com>
2021-06-27 11:23:29 +0000

    EOD commit

--- PeacefulPie/RayCasts.cs
+++ PeacefulPie/RayCasts.cs
@@ -28,4 +28,23 @@ public class RayCasts : MonoBehaviour {
 	[Range(0, 360)]
 	public int XTotalAngle = 60;
 	[Range(0, 360)]
-	[Tooltip("What is the tota
+	[Tooltip("What is the total vertical angle subtended by the rays? (60 is good default)")]
+	public int YTotalAngle = 60;
+	[Tooltip("Length of each ray")]
+	public int RayLength = 40;
+	[Tooltip("Ray radius. 0 means RayCast, >0 is SphereCast")]
+	public float RayRadius = 0;
+	public bool ShowRaysInEditor = true;
+	public bool ShowRaysInPlayer = false;
+	[Tooltip("Mandatory field: list here the tags you want to detect. Each will be given it's own output feature plane.")]
+	public List<string> DetectableTags = new List<string>();
+
+	class Ray {
+		public Vector3 direction;
+		public Ray(Vector3 direction) {
+			this.direction = direction;
+		}
+	}
+
+	Vector3 RayDirection(int xIdx, int yIdx) {
+		float x_angle = XResolution > 1 ? XTotalAngle / (XResolution - 1.0f) * xIdx - XTotalAn
