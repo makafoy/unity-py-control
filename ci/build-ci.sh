54cc3d6575b5a9a643c2e81e6b8d4128f77cad3c
makafoy <142866042+makafoy@users.noreply.github.com>
2021-07-03 17:47:15 +0000

    Создан проект PyCharm

--- ci/build-ci.sh
+++ ci/build-ci.sh
@@ -5,4 +5,6 @@ set -x
 sed -i -e 's/\/Applications\/Unity\/Hub\/Editor\/2021.3.16f1\/Unity.app\/Contents\/Managed\/UnityEngine/\/opt\/Unity\/Editor\/Data\/Managed\/UnityEngine/g' PeacefulPie/PeacefulPie.csproj
 cat PeacefulPie/PeacefulPie.csproj
 dotnet build PeacefulPie &&  \
-    cp PeacefulPie/bin/Debug/netstan
+    cp PeacefulPie/bin/Debug/netstandard2.1/PeacefulPie.dll .
+find . -cmin -1
+find . -cmin -1 -name 'PeacefulPie*'
