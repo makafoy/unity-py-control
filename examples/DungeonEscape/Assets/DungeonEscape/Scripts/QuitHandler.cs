cc5b9bf22b6b8e066aec8d6e2fac31de5f58e135
makafoy <142866042+makafoy@users.noreply.github.com>
2022-07-28 19:05:16 +0000

    repo init

--- examples/DungeonEscape/Assets/DungeonEscape/Scripts/QuitHandler.cs
+++ examples/DungeonEscape/Assets/DungeonEscape/Scripts/QuitHandler.cs
@@ -4,4 +4,17 @@ public class QuitHandler : MonoBehaviour
 {
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            this.Quit();
+        }
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit(0);
+    }
+}
