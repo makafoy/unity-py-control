c18e36932e942bce955c66cc106bdaa7ab9ebf83
makafoy <142866042+makafoy@users.noreply.github.com>
2022-08-13 06:40:25 +0000

    Remove some unnecessary code

---
+++ examples/DungeonEscape/Assets/PushBlock/Scripts/PushBlockSettings.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PushBlockSettings : MonoBehaviour
+{
+    /// <summary>
+    /// The "walking speed" of the agents in the scene.
+    /// </summary>
+    public float agentRunSpeed;
+
+    /// <summary>
+    /// The agent rot
