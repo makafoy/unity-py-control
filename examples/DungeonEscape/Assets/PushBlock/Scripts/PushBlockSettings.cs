90acaa89bdd166915286cc73bb861f2fe0958fcd
makafoy <142866042+makafoy@users.noreply.github.com>
2022-08-13 08:59:06 +0000

    Version 0.9

--- examples/DungeonEscape/Assets/PushBlock/Scripts/PushBlockSettings.cs
+++ examples/DungeonEscape/Assets/PushBlock/Scripts/PushBlockSettings.cs
@@ -8,4 +8,25 @@ public class PushBlockSettings : MonoBehaviour
     public float agentRunSpeed;
 
     /// <summary>
-    /// The agent rot
+    /// The agent rotation speed.
+    /// Every agent will use this setting.
+    /// </summary>
+    public float agentRotationSpeed;
+
+    /// <summary>
+    /// The spawn area margin multiplier.
+    /// ex: .9 means 90% of spawn area will be used.
+    /// .1 margin will be left (so players don't spawn off of the edge).
+    /// The higher this value, the longer training time required.
+    /// </summary>
+    public float spawnAreaMarginMultiplier;
+
+    /// <summary>
+    /// When a goal is scored the ground will switch to this
+    /// material for a few seconds.
+    /// </summary>
+    public Material goalScoredMaterial;
+
+    /// <summary>
+    /// When an agent fails, the ground will turn this material for a few seconds.
+    /// </su
