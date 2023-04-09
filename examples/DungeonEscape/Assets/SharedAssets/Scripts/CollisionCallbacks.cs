fb1d135a211de53592ded9bb1e1b391fa6ae02d4
makafoy <142866042+makafoy@users.noreply.github.com>
2023-04-09 12:47:44 +0000

    Release 0.4.0

--- examples/DungeonEscape/Assets/SharedAssets/Scripts/CollisionCallbacks.cs
+++ examples/DungeonEscape/Assets/SharedAssets/Scripts/CollisionCallbacks.cs
@@ -8,4 +8,21 @@ namespace Unity.MLAgentsExamples
     /// Add this script to the target you want the agent to touch.
     /// Callbacks will be triggered any time the target is touched with a collider tagged as 'tagToDetect'
     /// </summary>
-    publi
+    public class CollisionCallbacks : MonoBehaviour
+    {
+        //        [System.Serializable] public class BoolEvent : UnityEvent<bool> { }
+        //        [SerializeField] BoolEvent boolEvent = new BoolEvent();
+        //        public void OnBoolEvent(bool value)
+        //        {
+        //            Debug.Log($"OnBoolEvent {value}");
+        //        }
+
+
+        [Header("Collider Tag To Detect")]
+        public string tagToDetect = "agent"; //collider tag to detect
+
+        //        [Header("Target Placement")]
+        //        public float spawnRadius; //The radius in which a target can be randomly spawned.
+        //        public bool respawnIfTouched; //Should the target respawn to a different position when touched
+        //
+        //        [Header("Tar
