383f861b94d6271a34e6f0ba91f111674a77b7f3
makafoy <142866042+makafoy@users.noreply.github.com>
2023-06-19 22:45:24 +0000

    Fix #120

--- examples/DungeonEscape/python/mlflow_logging.py
+++ examples/DungeonEscape/python/mlflow_logging.py
@@ -16,4 +16,21 @@ class MLFlowLogger(KVWriter):
     """
 
     def __init__(
-        self, experiment_name: str, run_name: str, params_to_log: Di
+        self, experiment_name: str, run_name: str, params_to_log: Dict[str, Any]
+    ):
+        self.experiment_name = experiment_name
+        self.run_name = run_name
+        assert "MLFLOW_TRACKING_URI" in os.environ
+        print("mlflow tracking uri", mlflow.get_tracking_uri())
+        self.client = mlflow.MlflowClient()
+        self.experiment_id = mlflow.get_experiment_by_name(
+            experiment_name
+        ).experiment_id
+        if not self.experiment_id:
+            self.experiment_id = mlflow.create_experiment(experiment_name)
+        print("experiment id", self.experiment_id)
+        self.run = mlflow.start_run(experiment_id=self.experiment_id, run_name=run_name)
+
+        try:
+            # check we are in a repo
+            subprocess.check_output(["git", "status"], stderr=subproces
