7b8389b82b38c8f4562d45545d21b4e2e475ecf1
makafoy <142866042+makafoy@users.noreply.github.com>
2023-07-09 14:03:47 +0000

    Fix url.

--- examples/DungeonEscape/python/mlflow_utils.py
+++ examples/DungeonEscape/python/mlflow_utils.py
@@ -11,4 +11,21 @@ import mlflow.entities
 
 class MlflowLoader:
     """
-    Utilities for interac
+    Utilities for interacting with mlflow
+
+    When a run_name is provided as input, if there are multiple runs with the same name,
+    this class uses the run with the longest duration, or if there is a run currently running, with that
+    name, then uses that run.
+
+    You can either st MLFLOW_TRACKING_URI envornment variable with the url of your mlflow server, or
+    pass the url in through the parameter mlflow_uri.
+    """
+
+    def __init__(self, experiment_name: str, mlflow_uri: Optional[str] = None):
+        self.experiment_name = experiment_name
+        self.cache_dir = ".mlflow_cache"
+        if not os.path.exists(self.cache_dir):
+            os.makedirs(self.cache_dir)
+
+        if mlflow_uri is None:
+            assert "MLFLO
