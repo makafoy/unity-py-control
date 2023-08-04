d0a5137d0a0bcba9b7e4b5f1984dadae15ec2b9c
makafoy <142866042+makafoy@users.noreply.github.com>
2023-08-04 14:12:28 +0000

    Add change log entry

--- examples/DungeonEscape/python/my_unity_env.py
+++ examples/DungeonEscape/python/my_unity_env.py
@@ -6,4 +6,37 @@ import gym.spaces
 import numpy as np
 from numpy.typing import NDArray
 
-from peaceful_pie import 
+from peaceful_pie import ray_results_helper, unity_comms
+
+
+@dataclass
+class PlayerObservation:
+    IAmAlive: bool
+    IHaveAKey: bool
+    rayResults: ray_results_helper.RayResults
+
+
+@dataclass
+class RLResult:
+    reward: float
+    episodeFinished: bool
+    playerObservations: List[PlayerObservation]
+
+
+class MyUnityEnv(gym.Env):
+    def __init__(
+        self,
+        comms: unity_comms.UnityComms,
+    ):
+        self.comms = comms
+
+        self.action_space = gym.spaces.MultiDiscrete(
+            [4] * 3
+        )  # turn left/right look up/down forward
+        obs = self.reset()
+        print("obs.shape", obs.shape)
+        self.observation_space = gym.spaces.Box(
+            low=0, high=1, shape=obs.shape, dtype=np.float32
+        )
+
+    def r
