d76aac2af4e606aae2e463a3d4474690c47628ac
makafoy <142866042+makafoy@users.noreply.github.com>
2023-08-10 06:24:31 +0000

    Updated seeds

--- examples/DungeonEscape/python/rl1.py
+++ examples/DungeonEscape/python/rl1.py
@@ -17,3 +17,24 @@ from stable_baselines3.common.vec_env import SubprocVecEnv, VecEnv, VecMonitor
 from peaceful_pie.unity_comms import UnityComms
 
 
+def dump_params_counts(net: torch.nn.Module) -> int:
+    total_params = 0
+    for key, param in net.named_parameters():
+        print(key, param.numel())
+        total_params += param.numel()
+    print("total_params", total_params)
+    return total_params
+
+
+def run(args: argparse.Namespace) -> None:
+    class EnvFactory:
+        def __init__(self, port: int, server_executable_path: Optional[str]):
+            # I think this gets pickled into a new process
+            self.port = port
+            self.server_executable_path = server_executable_path
+            self.unity_comms = UnityComms(
+                port=self.port, server_executable_path=self.server_executable_path
+            )
+
+        def __call__(self) -> MyUnityEnv:
+            # I think this bit 
